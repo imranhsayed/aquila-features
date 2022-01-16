666a20e3bf78ab96a8112930421e8ae12cbe66fc
Imran <imranhsayed@gmail.com>
2022-01-16 14:53:12 +0530

    Add plugin file

---
+++ aquila-features.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Aquila Features
+ * Plugin URI:        https://codeytek.com/aquila-features/
+ * Description:       Adds Gutenberg Blocks.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Imran Sayed
+ * Author URI:        https://codeytek.com/about/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       aquila-features
+ * Domain Path:       /languages
+ */
+
