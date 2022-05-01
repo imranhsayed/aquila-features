9b96dc00c8dbd776dd18f2c35b31e815be889b3d
Imran <imranhsayed@gmail.com>
2022-05-01 13:48:05 +0530

    Fix phpcs errors

--- assets/build/assets.php
+++ assets/build/assets.php
@@ -1,1 +1,6 @@
-<?php return array('js/editor.js' => array('dependencies' => array('wp-polyfill'), 'version' => '556c664fa0ed4eb27a599e969901b9f7'));
+<?php return array(
+	'js/editor.js' => array(
+		'dependencies' => array( 'wp-polyfill' ),
+		'version'      => '556c664fa0ed4eb27a599e969901b9f7',
+	),
+);
